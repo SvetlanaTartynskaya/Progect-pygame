776b1bdadd77e19c769a7d4a5516ff6c088cde79
Светлана <137154922+SvetlanaTartynskaya@users.noreply.github.com>
2023-11-19 21:07:19 +0300

    Update Detective.py

--- Detective.py
+++ Detective.py
@@ -5,7 +5,7 @@ import sys
 pygame.init()
 
 # Задаем размеры окна
-win_width, win_height = 1280, 1024
+win_width, win_height = 800, 600
 
 # Создаем окно
 window = pygame.display.set_mode((win_width, win_height))
@@ -19,10 +19,27 @@ room_4_image = pygame.image.load("room4.jpg").convert()
 room_5_image = pygame.image.load("room5.jpg").convert()
 room_6_image = pygame.image.load("room6.jpg").convert()
 
+# Создание отдельных экземпляров класса Surface для каждой комнаты
+room_1_surface = pygame.Surface((win_width, win_height))
+room_1_surface.blit(room_1_image, (0, 0))
+
+room_2_surface = pygame.Surface((win_width, win_height))
+room_2_surface.blit(room_2_image, (0, 0))
+
+room_3_surface = pygame.Surface((win_width, win_height))
+room_3_surface.blit(room_3_image, (0, 0))
+
+room_4_surface = pygame.Surface((win_width, win_height))
+room_4_surface.blit(room_4_image, (0, 0))
+
+room_5_surface = pygame.Surface((win_width, win_height))
+room_5_surface.blit(room_5_image, (0, 0))
+
+room_6_surface = pygame.Surface((win_width, win_height))
+room_6_surface.blit(room_6_image, (0, 0))
+
 # Переменная для хранения текущей комнаты
-current_room = room_1_image
-room_num = 0
-main_rooms = (room_1_image, room_2_image, room_3_image, room_4_image)
+current_room = room_1_surface
 
 # Основной игровой цикл
 running = True
@@ -36,30 +53,32 @@ while running:
         elif event.type == pygame.KEYDOWN:
             # Переключение комнат с помощью стрелок
             if event.key == pygame.K_RIGHT:
-                if current_room in main_rooms:
-                    room_num += 1
-                    if room_num > 3:
-                        room_num = 0
-                    current_room = main_rooms[room_num]
+                if current_room == room_1_surface:
+                    current_room = room_2_surface
+                elif current_room == room_2_surface:
+                    current_room = room_3_surface
+                elif current_room == room_3_surface:
+                    current_room = room_4_surface
+                elif current_room == room_4_surface:
+                    current_room = room_5_surface
+                elif current_room == room_5_surface:
+                    current_room = room_6_surface
+                elif current_room == room_6_surface:
+                    current_room = room_1_surface
 
-            if event.key == pygame.K_LEFT:
-                if current_room in main_rooms:
-                    room_num -= 1
-                    if room_num < 0:
-                        room_num = 3
-                    current_room = main_rooms[room_num]
-
-            if event.key == pygame.K_UP:
-                if current_room in main_rooms:
-                    current_room = room_5_image
-                elif current_room == room_6_image:
-                    current_room = main_rooms[room_num]
-
-            if event.key == pygame.K_DOWN:
-                if current_room in main_rooms:
-                    current_room = room_6_image
-                elif current_room == room_5_image:
-                    current_room = main_rooms[room_num]
+            elif event.key == pygame.K_LEFT:
+                if current_room == room_2_surface:
+                    current_room = room_1_surface
+                elif current_room == room_3_surface:
+                    current_room = room_2_surface
+                elif current_room == room_4_surface:
+                    current_room = room_3_surface
+                elif current_room == room_5_surface:
+                    current_room = room_4_surface
+                elif current_room == room_6_surface:
+                    current_room = room_5_surface
+                elif current_room == room_1_surface:
+                    current_room = room_6_surface
 
     # Отрисовка текущей комнаты
     window.blit(current_room, (0, 0))
