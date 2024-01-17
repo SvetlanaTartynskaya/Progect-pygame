5929457878de9ee40e236f639b972d1acddb5591
Светлана <137154922+SvetlanaTartynskaya@users.noreply.github.com>
2024-01-17 21:12:47 +0300

    Update machinki.py

--- machinki.py
+++ machinki.py
@@ -13,20 +13,20 @@ black = (0, 0, 0)
 red = (255, 215, 0)
 
 # размеры окна.
-width = 550
-height = 400
+w = 550
+h = 400
 
 # загрузка изображений машин.
 cars = ('car2.jpg', 'car3.jpg', 'car4.jpg')
 cars_surf = []
 
 # создание окна.
-pov = pygame.display.set_mode((width, height))
+pov = pygame.display.set_mode((w, h))
 
 # текст после окончания игры.
 font = pygame.font.Font(None, 70)
-text = font.render("game over!", True, red)
-place = text.get_rect(center=(width // 2, height // 2))
+text = font.render("Вы не прошли!", True, red)
+place = text.get_rect(center=(w // 2, h // 2))
 pov.blit(text, place)
 pygame.display.update()
 
@@ -44,7 +44,7 @@ class Car(pygame.sprite.Sprite):
         self.speed = randint(1, 3)
 
     def update(self):
-        if self.rect.y < height:
+        if self.rect.y < h:
             self.rect.y += self.speed
         else:
             self.kill()
@@ -54,13 +54,13 @@ class UserCar(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('car.jpg').convert_alpha()
-        self.rect = self.image.get_rect(midbottom=(width // 2, height - 10))
+        self.rect = self.image.get_rect(midbottom=(w // 2, h - 10))
 
 # группа для хранения машин.
 cars_group = pygame.sprite.Group()
 
 # создание случайной машины.
-Car(randint(1, width), cars_surf[randint(0, 2)], cars_group)
+Car(randint(1, w), cars_surf[randint(0, 2)], cars_group)
 
 # создание управляемой машины.
 user_car = UserCar()
@@ -82,7 +82,7 @@ while play:
             sys.exit()
         elif event.type == pygame.USEREVENT:
             # появление новых машин.
-            Car(randint(1, width), cars_surf[randint(0, 2)], cars_group)
+            Car(randint(1, w), cars_surf[randint(0, 2)], cars_group)
     
     # отрисовка фона.
     pov.blit(road, [-128, 0])
@@ -121,7 +121,7 @@ while play:
 if game_over_lose:
     pov.fill(black)
     text = font.render("Вы проиграли!", True, red)
-    place = text.get_rect(center=(width // 2, height // 2))
+    place = text.get_rect(center=(w // 2, h // 2))
     pov.blit(text, place)
     pygame.display.update()
     pygame.time.delay(400)
@@ -129,7 +129,7 @@ if game_over_lose:
 if game_over_win:
     pov.fill(black)
     text = font.render("Вы прошли дальше!", True, red)
-    place = text.get_rect(center=(width // 2, height // 2))
+    place = text.get_rect(center=(w // 2, h // 2))
     pov.blit(text, place)
     pygame.display.update()
     pygame.time.delay(400)
