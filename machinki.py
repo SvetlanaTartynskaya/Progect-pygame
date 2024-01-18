c1dd33a0241397eaf11bd33f02216355c86e6b2d
Nenechya <148703925+Nenechya@users.noreply.github.com>
2024-01-18 23:08:28 +0300

    Update machinki.py

--- machinki.py
+++ machinki.py
@@ -1,6 +1,5 @@
 from random import randint
 import pygame
-import sys
 
 # инициализация библиотеки pygame.
 pygame.init()
@@ -16,6 +15,8 @@ red = (255, 215, 0)
 w = 550
 h = 400
 
+font = pygame.font.Font(None, 70)
+
 # загрузка изображений машин.
 cars = ('car2.jpg', 'car3.jpg', 'car4.jpg')
 cars_surf = []
@@ -23,16 +24,10 @@ cars_surf = []
 # создание окна.
 pov = pygame.display.set_mode((w, h))
 
-# текст после окончания игры.
-font = pygame.font.Font(None, 70)
-text = font.render("Вы не прошли!", True, red)
-place = text.get_rect(center=(w // 2, h // 2))
-pov.blit(text, place)
-pygame.display.update()
-
 # загрузка изображений машин.
 for i in range(len(cars)):
     cars_surf.append(pygame.image.load(cars[i]).convert_alpha())
+
 
 # класс для машин.
 class Car(pygame.sprite.Sprite):
@@ -49,6 +44,7 @@ class Car(pygame.sprite.Sprite):
         else:
             self.kill()
 
+
 # класс для управляемой машины.
 class UserCar(pygame.sprite.Sprite):
     def __init__(self):
@@ -56,80 +52,78 @@ class UserCar(pygame.sprite.Sprite):
         self.image = pygame.image.load('car.jpg').convert_alpha()
         self.rect = self.image.get_rect(midbottom=(w // 2, h - 10))
 
+
 # группа для хранения машин.
 cars_group = pygame.sprite.Group()
-
-# создание случайной машины.
-Car(randint(1, w), cars_surf[randint(0, 2)], cars_group)
-
-# создание управляемой машины.
-user_car = UserCar()
-
-# флаг для проверки продолжения игры.
-play = True
-game_over_lose = False
-game_over_win = False
 
 # загрузка фона.
 road = pygame.image.load('road.jpg')
 
-# счетчик времени и флаг для проверки достижения 40 секунд.
-time_counter = 0
 
-while play:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.USEREVENT:
-            # появление новых машин.
-            Car(randint(1, w), cars_surf[randint(0, 2)], cars_group)
-    
-    # отрисовка фона.
-    pov.blit(road, [-128, 0])
+def main():
+    play = True
+    game_over_win = False
+    game_over_lose = False
+    font = pygame.font.Font(None, 70)
+    pov = pygame.display.set_mode((w, h))
+    pygame.time.set_timer(pygame.USEREVENT, 1000)
+    Car(randint(1, w), cars_surf[randint(0, 2)], cars_group)
+    user_car = UserCar()
+    time_counter = 0
+    while play:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                play = False
+            elif event.type == pygame.USEREVENT:
+                # появление новых машин.
+                Car(randint(1, w), cars_surf[randint(0, 2)], cars_group)
 
-    # отрисовка и обновление машин.
-    cars_group.draw(pov)
-    cars_group.update()
+        # отрисовка фона.
+        pov.blit(road, [-128, 0])
 
-    # обработка нажатий клавиш.
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        user_car.rect.x -= 2
-    elif keys[pygame.K_RIGHT]:
-        user_car.rect.x += 2
-    
-    pov.blit(user_car.image, user_car.rect)
+        # отрисовка и обновление машин.
+        cars_group.draw(pov)
+        cars_group.update()
 
-    pygame.display.update()
-    pygame.time.delay(10)
-    
-    # проверка на столкновение с машинами.
-    if pygame.sprite.spritecollideany(user_car, cars_group):
-        play = False
-        game_over_lose = True
-        pov.blit(text, place)
+        # обработка нажатий клавиш.
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and user_car.rect.x > 0:
+            user_car.rect.x -= 2
+        if keys[pygame.K_RIGHT] and user_car.rect.bottomright[0] < w:
+            user_car.rect.x += 2
+        if keys[pygame.K_UP] and user_car.rect.y > (h // 2):
+            user_car.rect.y -= 2
+        if keys[pygame.K_DOWN] and user_car.rect.bottomright[1] < h:
+            user_car.rect.y += 2
+
+        pov.blit(user_car.image, user_car.rect)
+
         pygame.display.update()
-        pygame.time.delay(20)
+        pygame.time.delay(10)
 
-    time_counter += 1
+        if game_over_win:
+            pov.fill(black)
+            text = font.render("Вы прошли дальше!", True, red)
+            place = text.get_rect(center=(w // 2, h // 2))
+            pov.blit(text, place)
+            pygame.display.update()
+            pygame.time.delay(400)
 
-    if time_counter >= 2000:
-        play = False
-        game_over_win = True
+        # проверка на столкновение с машинами.
+        if pygame.sprite.spritecollideany(user_car, cars_group):
+            game_over_lose = True
+            if game_over_lose:
+                play = False
+                pov.fill(black)
+                text = font.render("Вы проиграли!", True, red)
+                place = text.get_rect(center=(w // 2, h // 2))
+                pov.blit(text, place)
+                pygame.display.update()
+                cars_group.empty()
+                pygame.time.delay(400)
 
+        time_counter += 1
 
-if game_over_lose:
-    pov.fill(black)
-    text = font.render("Вы проиграли!", True, red)
-    place = text.get_rect(center=(w // 2, h // 2))
-    pov.blit(text, place)
-    pygame.display.update()
-    pygame.time.delay(400)
-
-if game_over_win:
-    pov.fill(black)
-    text = font.render("Вы прошли дальше!", True, red)
-    place = text.get_rect(center=(w // 2, h // 2))
-    pov.blit(text, place)
-    pygame.display.update()
-    pygame.time.delay(400)
+        if time_counter >= 2000:
+            play = False
+            game_over_win = True
